21eb94bd9bd2819179bb0b93a1b2c9a3e0f47e9a
u06754010-sudo <u06754010@gmail.com>
2026-04-06 22:01:01 -0500

    Proyecto autos web base

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+@section('content')
+
+<div class="container mt-5 text-center">
+
+<h1>Bienvenido a Autos Web</h1>
+
+<p class="mt-3">
+En nuestra página encontrarás información sobre algunos de los autos deportivos más impresionantes del mundo.
+Explora nuestros modelos y conoce sus características.
+</p>
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu55CNL_I2oJqboLOddBef-M-5wDb4ysB_pQ&s" class="img-fluid mt-4" width="500">
+
+</div>
+
+@endsection
